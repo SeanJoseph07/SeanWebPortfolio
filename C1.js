5db9ba13b2e173e710455850be0f72cd74b0aa5f
Sean <josephseankyle@gmail.com>
2022-04-22 18:52:52 +0800

    test

---
+++ C1.js
@@ -0,0 +1,48 @@
+// Words typing
+		var typed = new Typed(".say", {
+		    strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+		    typeSpeed: 100,
+		    backSpeed: 60,
+		    loop: true
+		});
+
+// var i = 0;
+// var txt = 'Lorem ipsum typing effect!'; /* The text */
+// var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+		// $('#example').popover(options)
+
+		// $('.popover-dismiss').popover({
+		//   trigger: 'focus'
+		// })
+
+
+
+
+// $(function () {        
+
+//     $("#contactform").validate({
+//         rules: {
+//             Name: {
+//                 required: true,
+//                 minlength: 8
+//             },
+//             action: "required"
+//         },
+//         messages: {
+//             Name: {
+//                 required: "Please enter some data",
+//                 minlength: "Your data must be at least 8 characters"
+//             },
+//             action: "Please provide some data"
+//         }
+//     });
+// });
